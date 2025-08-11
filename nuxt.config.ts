0dec9e9aca2692678b6d1a6ca398c448cf70a8cb
lumaa-dev <lumaa@lumaa.fr>
2025-08-11 13:37:49 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: ["motion-v/nuxt", "nuxt-countdown"]
+  modules: ["motion-v/nuxt", "nuxt-countdown"],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/hero_dark.png"
+        }
+      ]
+    }
+  }
 })
